933573fbe0688dc850308e58388b7d09dbde0898
Vedran <vedranknezevic1@yahoo.com>
2019-08-27 21:08:03 +0200

    pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,15 +6,33 @@ const PDFDocument = require("pdfkit");
 const Product = require("../models/product");
 const Order = require("../models/order");
 
+const ITEMS_PER_PAGE = 3;
+
 exports.getProducts = (req, res, next) => {
-  console.log("PRODUCTS");
+  const page = +req.query.page || 1;
+  console.log(page);
+
+  let totalItems;
   Product.find()
+    .countDocuments()
+    .then(numOfProducts => {
+      totalItems = numOfProducts;
+
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
-      console.log(products);
       res.render("shop/product-list", {
         prods: products,
-        pageTitle: "All Products",
-        path: "/products"
+        pageTitle: "Product List",
+        path: "/products",
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
     .catch(err => {
@@ -45,12 +63,30 @@ exports.getProduct = (req, res, next) => {
 
 exports.getIndex = (req, res, next) => {
   console.log("INDEX PAGE");
+  const page = +req.query.page || 1;
+  console.log(page);
+
+  let totalItems;
   Product.find()
+    .countDocuments()
+    .then(numOfProducts => {
+      totalItems = numOfProducts;
+
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
       res.render("shop/index", {
         prods: products,
         pageTitle: "Shop",
-        path: "/"
+        path: "/",
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
     .catch(err => {
